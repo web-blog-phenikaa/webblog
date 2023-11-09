91c3555acb5c28da7109b9e60b40b78e85a7f876
DaiNghiadz~ <nghiabeo1605gmail.com0>
2023-11-09 16:54:44 +0700

    done

--- js/add-edit-post.js
+++ js/add-edit-post.js
@@ -1,74 +1,74 @@
-import postApi from './api/postApi'
-import { initForm, toast } from './utils'
+import postApi from "./api/postApi";
+import { initForm, toast } from "./utils";
 
 function removeUnused(formValue) {
-  const payload = { ...formValue }
+    const payload = { ...formValue };
 
-  if (formValue.imageSource === 'picsum') {
-    delete payload.image
-  } else {
-    delete payload.imageUrl
-  }
+    if (formValue.imageSource === "picsum") {
+        delete payload.image;
+    } else {
+        delete payload.imageUrl;
+    }
 
-  if (!payload.id) delete payload.id
+    if (!payload.id) delete payload.id;
 
-  delete payload.imageSource
+    delete payload.imageSource;
 
-  return payload
+    return payload;
 }
 
 function jsonToFormData(payload) {
-  const formData = new FormData()
+    const formData = new FormData();
 
-  for (const key in payload) {
-    formData.set(key, payload[key])
-  }
+    for (const key in payload) {
+        formData.set(key, payload[key]);
+    }
 
-  console.log(formData)
+    console.log(formData);
 
-  return formData
+    return formData;
 }
 
 async function handleSubmitPost(formValue) {
-  try {
-    const payload = removeUnused(formValue)
-    console.log(payload)
-    const formData = jsonToFormData(payload)
-    console.log(formData)
+    try {
+        const payload = removeUnused(formValue);
+        console.log(payload);
+        const formData = jsonToFormData(payload);
+        console.log(formData);
 
-    const savePost = formValue.id
-      ? await postApi.updateFormData(formData)
-      : await postApi.addFormData(formData)
-    // call Api add or edit
-    // show message
-    toast.success('savePost success')
-    //redirect home
-    setTimeout(() => {
-      window.location.assign(`/post-detail.html?id=${savePost.id}`)
-    }, 5000)
-  } catch (error) {}
+        const savePost = formValue.id
+            ? await postApi.updateFormData(formData)
+            : await postApi.addFormData(formData);
+        // call Api add or edit
+        // show message
+        toast.success("savePost success");
+        //redirect home
+        setTimeout(() => {
+            window.location.assign(`/post-detail.html?id=${savePost.id}`);
+        }, 5000);
+    } catch (error) {}
 }
 
-;(async () => {
-  try {
-    const params = new URLSearchParams(window.location.search)
+(async () => {
+    try {
+        const params = new URLSearchParams(window.location.search);
 
-    // ưu tiên viết code cho người đọc code sau phát hiểu đang làm cái gì luôn
+        // ưu tiên viết code cho người đọc code sau phát hiểu đang làm cái gì luôn
 
-    const defaultValues = Boolean(params.get('id'))
-      ? await postApi.getById(params.get('id'))
-      : {
-          title: '',
-          description: '',
-          author: '',
-          imageUrl: '',
-        }
-    initForm({
-      formId: 'postForm',
-      defaultValues,
-      onSubmit: handleSubmitPost,
-    })
-  } catch (error) {
-    console.log(error)
-  }
-})()
+        const defaultValues = Boolean(params.get("id"))
+            ? await postApi.getById(params.get("id"))
+            : {
+                  title: "",
+                  description: "",
+                  author: "",
+                  imageUrl: "",
+              };
+        initForm({
+            formId: "postForm",
+            defaultValues,
+            onSubmit: handleSubmitPost,
+        });
+    } catch (error) {
+        console.log(error);
+    }
+})();
--- js/utils/index.js
+++ js/utils/index.js
@@ -2,6 +2,6 @@ export * from "./common";
 export * from "./post";
 export * from "./pagination";
 export * from "./search";
-// export * from './lightbox'
-// export * from './post-form'
-// export * from './toast'
+export * from "./lightbox";
+export * from "./post-form";
+export * from "./toast";
